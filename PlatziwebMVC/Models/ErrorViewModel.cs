9de28ba72f8df1d05dc9e0fcc118cc6df5758a49
Marcelo <gorski.marcelo@gmail.com>
2024-01-24 12:29:43 -0300

    Primer Commit

---
+++ PlatziwebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlatziwebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
